fd92a607c460d47fd7e4444f5c8dea872a43ae80
nazgull08 <nazgull08@protonmail.com>
2023-02-10 01:25:36 +0300

    added generic type

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,45 +1,42 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct Vertex{
-    pub id      : u32,
-    pub payload : u32,
-}
-
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct Edge{
-    pub id         : u32,
-    pub payload    : u32,
-    pub vertex_start : u32,
-    pub vertex_end   : u32,
-    pub oriented   : bool
-}
-
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct MetaVertex{
-    pub id : i32,
-    pub payload : i32,
-    pub metagraph : Option<MetaGraph>
-}
-
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct MetaEdge{
-    pub id : i32,
-    pub payload      : u32,
-    pub vertex_start : u32,
-    pub vertex_end   : u32,
-    pub metagraph : Option<MetaGraph>,
-    pub oriented   : bool
+pub struct Vertex<T> {
+    pub id: u32,
+    pub payload: T,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct MetaGraph{
-    pub id : i32,
-    pub vertecies : Vec<Vertex>,
-    pub meta_vertecies : Vec<MetaVertex>,
-    pub edges : Vec<Edge>,
-    pub meta_edges : Vec<MetaEdge>,
+pub struct Edge<T> {
+    pub id: u32,
+    pub payload: T,
+    pub vertex_start: u32,
+    pub vertex_end: u32,
+    pub oriented: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct MetaVertex<T> {
+    pub id: i32,
+    pub payload: T,
+    pub metagraph: Option<MetaGraph<T>>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct MetaEdge<T> {
+    pub id: i32,
+    pub payload: T,
+    pub vertex_start: u32,
+    pub vertex_end: u32,
+    pub metagraph: Option<MetaGraph<T>>,
+    pub oriented: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct MetaGraph<T> {
+    pub id: i32,
+    pub vertecies: Vec<Vertex<T>>,
+    pub meta_vertecies: Vec<MetaVertex<T>>,
+    pub edges: Vec<Edge<T>>,
+    pub meta_edges: Vec<MetaEdge<T>>,
 }
