b69d75e700fc92b9197ead3f2a63778b97390f94
nazgull08 <nazgull08@protonmail.com>
2023-02-10 21:26:44 +0300

    fix

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -17,14 +17,14 @@ pub struct Edge<T> {
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct MetaVertex<T> {
-    pub id: i32,
+    pub id: u32,
     pub payload: T,
     pub metagraph: Option<MetaGraph<T>>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct MetaEdge<T> {
-    pub id: i32,
+    pub id: u32,
     pub payload: T,
     pub vertex_start: u32,
     pub vertex_end: u32,
@@ -34,9 +34,16 @@ pub struct MetaEdge<T> {
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct MetaGraph<T> {
-    pub id: i32,
+    pub id: u32,
     pub vertecies: Vec<Vertex<T>>,
     pub meta_vertecies: Vec<MetaVertex<T>>,
     pub edges: Vec<Edge<T>>,
     pub meta_edges: Vec<MetaEdge<T>>,
+}
+
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum PayloadWrapper {
+    PString(String),
+    Pi32(i32),
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,18 @@
-use crate::types::{MetaGraph, MetaVertex, MetaEdge};
+use crate::types::{MetaGraph, MetaVertex, MetaEdge, PayloadWrapper};
 
 
-pub fn example_metagraph() -> MetaGraph<i32>{
-let a = MetaVertex{ id: 0, payload: 10, metagraph: None};
-    let b = MetaVertex { id: 1, payload: 20, metagraph: None };
+pub fn example_metagraph() -> MetaGraph<PayloadWrapper>{
+let a = MetaVertex{ id: 0, payload: PayloadWrapper::Pi32(2), metagraph: None};
+    let b = MetaVertex { id: 1, payload:PayloadWrapper::Pi32(5), metagraph: None };
     let e1 = MetaEdge {
         id: 0,
-        payload: "a",
+        payload:PayloadWrapper::PString("small edge".to_owned()) ,
         vertex_start: a.id,
         vertex_end: b.id,
         oriented: false,
         metagraph: None,
     };
+    let mvs = vec![a,b];
+    let mes = vec![e1];
+    return MetaGraph{ id: 0, vertecies: vec![], meta_vertecies: mvs, edges: vec![], meta_edges: mes};
 }
